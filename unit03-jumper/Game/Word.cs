77cba331e42b1d923f2c51ccd07983e50617355e
lfacer <lfacer769@gmail.com>
2022-05-23 10:02:28 -0600

    5/23 changes

---
+++ unit03-jumper/Game/Word.cs
@@ -0,0 +1,61 @@
+using System;
+
+
+namespace Unit03.Game
+{
+    // TODO: Implement the Seeker class as follows...
+
+    // 1) Add the class declaration. Use the following class comment.
+
+        /// <summary>
+        /// <para>The person looking for the Hider.</para>
+        /// <para>
+        /// The responsibility of a Seeker is to keep track of its location.
+        /// </para>
+        /// </summary>
+        
+        public class Word
+        {
+
+            private string random_word = "";
+            public Word()
+            {
+                random_word = "apple";
+            }
+
+            public string GetLocation()
+            {
+                return random_word;
+            }
+
+            public void CurrentLetter(string letter)
+            {
+                this.random_word = letter;
+            }
+
+        }
+
+
+    // 2) Create the class constructor. Use the following method comment.
+        
+        /// <summary>
+        /// Constructs a new instance of Seeker.
+        /// </summary>
+       
+
+    // 3) Create the GetLocation() method. Use the following method comment.
+        
+        /// <summary>
+        /// Gets the current location.
+        /// </summary>
+        /// <returns>The current location as an int.</returns>
+        
+
+    // 4) Create the MoveLocation(int location) method. Use the following method comment.
+        
+        /// <summary>
+        /// Moves to the given location.
+        /// </summary>
+        /// <param name="location">The given location.</param>
+    
+}
